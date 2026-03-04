0f31dfc16292b564210c285ed7e8afc749aceba0
dracotato <160530153+DracoTato@users.noreply.github.com>
2026-03-05 00:13:31 +0200

    feat: solve ps4c [3/3 of ps4]

--- mit6_100l_f22_ps4_code/1_ps4/ps4c.py
+++ mit6_100l_f22_ps4_code/1_ps4/ps4c.py
@@ -1,13 +1,15 @@
 # Problem Set 4C
-# Name:
-# Collaborators:
+# Name: dracotato
+# Collaborators: N/A
 
 import json
-import ps4b # Importing your work from Part B
+
+import ps4b  # Importing your work from Part B
+
 
 ### HELPER CODE ###
 def load_words(file_name):
-    '''
+    """
     file_name (string): the name of the file containing
     the list of words to load
 
@@ -15,18 +17,18 @@ def load_words(file_name):
 
     Depending on the size of the word list, this function may
     take a while to finish.
-    '''
+    """
     # inFile: file
-    with open(file_name, 'r') as inFile:
+    with open(file_name, "r") as inFile:
         # wordlist: list of strings
         wordlist = []
         for line in inFile:
-            wordlist.extend([word.lower() for word in line.split(' ')])
+            wordlist.extend([word.lower() for word in line.split(" ")])
         return wordlist
 
 
 def is_word(word_list, word):
-    '''
+    """
     Determines if word is a valid word, ignoring
     capitalization and punctuation
 
@@ -40,7 +42,7 @@ def is_word(word_list, word):
     True
     >>> is_word(word_list, 'asdf') returns
     False
-    '''
+    """
     word = word.strip(" !@#$%^&*()-_+={}[]|\:;'<>?,./\"").lower()
     return word in word_list
 
@@ -56,16 +58,16 @@ def get_story_string():
 
 
 def get_story_pads():
-    with open('pads.txt') as json_file:
+    with open("pads.txt") as json_file:
         return json.load(json_file)
 
 
-WORDLIST_FILENAME = 'words.txt'
+WORDLIST_FILENAME = "words.txt"
 ### END HELPER CODE ###
 
 
 def decrypt_message_try_pads(ciphertext, pads):
-    '''
+    """
     Given a string ciphertext and a list of possible pads
     used to create it find the pad used to create the ciphertext
 
@@ -79,24 +81,38 @@ def decrypt_message_try_pads(ciphertext, pads):
         to encrypt the ciphertext
 
     Returns: (PlaintextMessage) A message with the decrypted ciphertext and the best pad
-    '''
-    raise NotImplementedError  # delete this line and replace with your code here
+    """
+    word_list = load_words(WORDLIST_FILENAME)
+    highest_score = ("", 0)
+
+    for pad in pads:
+        plaintext = ciphertext.decrypt_message(pad)
+        words = plaintext.get_text().split()
+        words = words if isinstance(words, list) else [words]
+        score = sum([int(is_word(word_list, word)) for word in words])
+
+        if score >= highest_score[1]:
+            highest_score = (plaintext, score)
+
+    return highest_score[0]
 
 
 def decode_story():
-    '''
+    """
     Write your code here to decode Bob's story using a list of possible pads
     Hint: use the helper functions get_story_string and get_story_pads and your EncryptedMessage class.
 
     Returns: (string) the decoded story
 
-    '''
-    raise NotImplementedError  # delete this line and replace with your code here
+    """
+    return decrypt_message_try_pads(
+        ps4b.EncryptedMessage(get_story_string()), get_story_pads()
+    )
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # # Uncomment these lines to try running decode_story()
     # story = decode_story()
     # print("Decoded story: ", story)
     pass
+
